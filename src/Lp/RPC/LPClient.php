e553935dda4b4adea8e14201d906758567ae4c95
Krasimir Krastev <krasimir@littlepostman.com>
2014-08-26 10:34:34 +0200

    [#versionBump] - v1.0.37

--- src/Lp/RPC/LPClient.php
+++ src/Lp/RPC/LPClient.php
@@ -14,7 +14,7 @@ class Lp_RPC_LPClient
     /**
      * @const LP_API_VERSION
      */
-    const LP_API_VERSION = '1.0.36';
+    const LP_API_VERSION = '1.0.37';
 
     /**
      * @const LP_SERVER_PRODUCTION
